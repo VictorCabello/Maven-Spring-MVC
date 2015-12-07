4a813d90fc15f07a6d0a9d4a12a34e46f8267574
Victor Cabello <vmeca87@gmail.com>
2015-12-06 19:39:28 -0600

    Error to test jenkins

--- src/main/java/com/richardhoppes/example/controller/HomeController.java
+++ src/main/java/com/richardhoppes/example/controller/HomeController.java
@@ -11,9 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class HomeController {
 
